d8fcd335084591d0d21d1c29f06a36e9551a02ed
Rickerd1234 <rick.v.oosterhout@gmail.com>
2025-03-25 19:09:40 +0100

    Simplified projection_union Proof

--- RelationalAlgebra/EquivRules.lean
+++ RelationalAlgebra/EquivRules.lean
@@ -4,55 +4,9 @@ open RM
 
 @[simp ←]
 theorem projection_union {s1 s2 : RelationSchema} (inst1 inst2 : RelationInstance s1) (h : s2 ⊆ s1):
-  projection (union inst1 inst2) s2 h = union (projection inst1 s2 h) (projection inst2 s2 h) := by
-    simp [projection, union]
-    ext x : 1
-    simp_all only [Set.mem_setOf_eq, Set.mem_union]
-    apply Iff.intro
-    · intro a
-      obtain ⟨w, h_1⟩ := a
-      obtain ⟨left, right⟩ := h_1
-      simp_all only
-      cases left with
-      | inl h_1 =>
-        apply Or.inl
-        apply Exists.intro
-        · apply And.intro
-          on_goal 2 => {
-            intro a b
-            rfl
-          }
-          · simp_all only
-      | inr h_2 =>
-        apply Or.inr
-        apply Exists.intro
-        · apply And.intro
-          on_goal 2 => {
-            intro a b
-            rfl
-          }
-          · simp_all only
-    · intro a
-      cases a with
-      | inl h_1 =>
-        obtain ⟨w, h_1⟩ := h_1
-        obtain ⟨left, right⟩ := h_1
-        simp_all only
-        apply Exists.intro
-        · apply And.intro
-          on_goal 2 => {
-            intro a b
-            rfl
-          }
-          · simp_all only [true_or]
-      | inr h_2 =>
-        obtain ⟨w, h_1⟩ := h_2
-        obtain ⟨left, right⟩ := h_1
-        simp_all only
-        apply Exists.intro
-        · apply And.intro
-          on_goal 2 => {
-            intro a b
-            rfl
-          }
-          · simp_all only [or_true]
+  projection (union inst1 inst2) s2 h = union (projection inst1 s2 h) (projection inst2 s2 h) :=
+    Set.ext λ _ => Iff.intro
+      -- w ∈ projection (union inst1 inst2) s2 h → w ∈ union (projection inst1 s2 h) (projection inst2 s2 h)
+      (λ ⟨w, hw_union, hw_project⟩ => Or.elim hw_union (λ w_in_1 => Or.inl ⟨w, w_in_1, hw_project⟩) (λ w_in_2 => Or.inr ⟨w, w_in_2, hw_project⟩))
+      -- w ∈ union (projection inst1 s2 h) (projection inst2 s2 h) → w ∈ projection (union inst1 inst2) s2 h
+      (λ hx => Or.elim hx (λ ⟨w, w_in_1, w_project⟩ => ⟨w, Or.inl w_in_1, w_project⟩) (λ ⟨w, w_in_2, w_project⟩ => ⟨w, Or.inr w_in_2, w_project⟩))
