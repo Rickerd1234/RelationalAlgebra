f75485d3c55096ec47869842d26278e0b5f2e2c8
Rickerd1234 <rick.v.oosterhout@gmail.com>
2025-06-30 20:56:35 +0200

    Updated PFun.ran helpers

--- RelationalAlgebra/Util/PFunFinRan.lean
+++ RelationalAlgebra/Util/PFunFinRan.lean
@@ -2,7 +2,13 @@ import Mathlib.Data.PFun
 import Mathlib.Data.Fintype.Sets
 import Mathlib.Data.Finset.PImage
 
-variable {α β : Type} [DecidableEq α] [DecidableEq β] (f : PFun α β) [Fintype f.Dom]
+variable {α β : Type} (f : PFun α β)
+
+@[simp]
+theorem ran_mem {b : β} : (∃a, f a = Part.some b) ↔ b ∈ f.ran
+    := by simp_all only [Part.eq_some_iff, PFun.ran, Set.mem_setOf_eq]
+
+variable [DecidableEq α] [DecidableEq β] [Fintype f.Dom]
 
 @[simp]
 instance (a : α) : Decidable (f a).Dom := by
@@ -10,7 +16,7 @@ instance (a : α) : Decidable (f a).Dom := by
   exact f.Dom.decidableMemOfFintype a
 
 @[simp]
-theorem ran_def : f.ran = f.Dom.toFinset.pimage f := by
+theorem ran_def_finset : f.ran = f.Dom.toFinset.pimage f := by
   ext x
   simp_all only [PFun.ran, PFun.Dom]
   simp [Part.dom_iff_mem, PFun.image_def]
@@ -18,5 +24,5 @@ theorem ran_def : f.ran = f.Dom.toFinset.pimage f := by
 
 @[simp]
 instance : Fintype f.ran := by
-  rw [ran_def]
+  rw [ran_def_finset]
   exact FinsetCoe.fintype (Finset.pimage f f.Dom.toFinset)
