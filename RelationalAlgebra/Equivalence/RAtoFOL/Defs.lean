eaf573841e49acdb18e15ff7a885bb28d61c05ff
Rickerd1234 <rick.v.oosterhout@gmail.com>
2025-08-01 14:36:06 +0200

    Initial selection

--- RelationalAlgebra/Equivalence/RAtoFOL/Defs.lean
+++ RelationalAlgebra/Equivalence/RAtoFOL/Defs.lean
@@ -1,31 +1,33 @@
 import RelationalAlgebra.Equivalence.RAtoFOL.Operations
+import RelationalAlgebra.FOL.QueryAux
 
 open RM
 
 noncomputable def ra_to_fol_query_def [FOL.folStruc] (raQ : RA.Query) (dbs : DatabaseSchema) : FOL.Query :=
   match raQ with
   | .R rn => .R dbs rn (FOL.outVar ∘ att_to_var ∘ (dbs rn).fromIndex)
-  | .s a b pos sq => (ra_to_fol_query_def sq dbs) -- a b pos @TODO
+  | .s a b pos sq => (ra_to_fol_query_def sq dbs).relabel (λ a' => Sum.inl (ite (a' = att_to_var b) (att_to_var a) a'))
   | .p rs sq => ra_to_fol_query_p (ra_to_fol_query_def sq dbs) rs
   | .j sq1 sq2 => sorry --ra_to_fol_query_j (ra_to_fol_query_def sq1 dbs) (ra_to_fol_def sq2 dbs) @TODO
   | .r f sq => ra_to_fol_query_r (ra_to_fol_query_def sq dbs) f
 
-noncomputable def ra_to_fol_outFn_def [FOL.folStruc] (raQ : RA.Query) (dbs : DatabaseSchema) (h : raQ.isWellTyped dbs) : Attribute →. FOL.Variable :=
+noncomputable def ra_to_fol_outFn_def [FOL.folStruc] (raQ : RA.Query) (dbs : DatabaseSchema) : Attribute →. FOL.Variable :=
   match raQ with
   | .R rn => PFun.res att_to_var (dbs rn)
-  | .s a b pos sq => ra_to_fol_outFn_def sq dbs (by simp_all [RA.Query.isWellTyped])
-  | .p rs sq => ra_to_fol_outFn_p (ra_to_fol_outFn_def sq dbs (by simp_all [RA.Query.isWellTyped])) rs
+  | .s a b pos sq => (λ a' => (ra_to_fol_outFn_def sq dbs) (ite (a' = b) a a'))
+  | .p rs sq => ra_to_fol_outFn_p (ra_to_fol_outFn_def sq dbs) rs
   | .j sq1 sq2 => sorry -- @TODO
-  | .r f sq => ra_to_fol_outFn_r (ra_to_fol_outFn_def sq dbs (by simp_all [RA.Query.isWellTyped])) f
+  | .r f sq => ra_to_fol_outFn_r (ra_to_fol_outFn_def sq dbs) f
 
-theorem ra_to_fol_outFn_eq_schema [FOL.folStruc] {raQ : RA.Query} {dbs} {h : raQ.isWellTyped dbs} :
-  (ra_to_fol_outFn_def raQ dbs h).Dom = raQ.schema dbs := by
+theorem ra_to_fol_outFn_eq_schema [FOL.folStruc] {raQ : RA.Query} {dbs} (h : raQ.isWellTyped dbs) :
+  (ra_to_fol_outFn_def raQ dbs).Dom = raQ.schema dbs := by
     induction raQ
     all_goals (
       simp [RA.Query.isWellTyped] at h
       simp_all [ra_to_fol_outFn_def, RA.Query.schema]
     )
     case R rn => rfl
+    case s a b pos sq ih => sorry
     case p rs sq ih =>
       ext a
       simp_all only [PFun.mem_dom, Finset.mem_coe, ra_to_fol_outFn_p]
@@ -37,7 +39,7 @@ theorem ra_to_fol_outFn_eq_schema [FOL.folStruc] {raQ : RA.Query} {dbs} {h : raQ
         next h_1 => simp_all only
         next h_1 => simp_all only [RelationSchema.index?_none, Part.not_mem_none]
       · intro a_1
-        have z : a ∈ (ra_to_fol_outFn_def sq dbs left).Dom := by aesop
+        have z : a ∈ (ra_to_fol_outFn_def sq dbs).Dom := by aesop
         simp only [Finset.mem_coe, ↓reduceIte, a_1]
         exact Part.dom_iff_mem.mp z
     case j sq1 sq2 ih1 ih2 => sorry -- @TODO
@@ -57,15 +59,15 @@ theorem ra_to_fol_outFn_eq_schema [FOL.folStruc] {raQ : RA.Query} {dbs} {h : raQ
         subst right_1
         simp_all only [inv_f_id_apply]
 
-theorem ra_to_fol_outFn_schema_mem_iff [FOL.folStruc] {att : Attribute} {raQ : RA.Query} {dbs} {h : raQ.isWellTyped dbs} :
-  att ∈ (ra_to_fol_outFn_def raQ dbs h).Dom ↔ att ∈ raQ.schema dbs := by
-    simp only [ra_to_fol_outFn_eq_schema, Finset.mem_coe]
+theorem ra_to_fol_outFn_schema_mem_iff [FOL.folStruc] {att : Attribute} {raQ : RA.Query} {dbs} (h : raQ.isWellTyped dbs) :
+  att ∈ (ra_to_fol_outFn_def raQ dbs).Dom ↔ att ∈ raQ.schema dbs := by
+    simp only [ra_to_fol_outFn_eq_schema, Finset.mem_coe, h]
 
 open FirstOrder Language
 
 noncomputable def ra_to_fol_def [FOL.folStruc] {dbs} (raQ : RA.Query) (h : raQ.isWellTyped dbs) : FOL.EvaluableQuery dbs := ⟨
   ra_to_fol_query_def raQ dbs,
-  ra_to_fol_outFn_def raQ dbs h,
+  ra_to_fol_outFn_def raQ dbs,
   by
     induction raQ
     all_goals (
@@ -76,9 +78,7 @@ noncomputable def ra_to_fol_def [FOL.folStruc] {dbs} (raQ : RA.Query) (h : raQ.i
     case s a b pos sq ih =>
       obtain ⟨left, right⟩ := h
       obtain ⟨left_1, right⟩ := right
-      cases b with
-      | inl val => apply ih
-      | inr val_1 => apply ih
+      sorry
     case p rs sq ih =>
       obtain ⟨left, right⟩ := h
       apply Fintype.ofFinset rs
@@ -87,7 +87,7 @@ noncomputable def ra_to_fol_def [FOL.folStruc] {dbs} (raQ : RA.Query) (h : raQ.i
       apply Iff.intro
       · intro a
         simp_all only [↓reduceIte, ← Part.dom_iff_mem]
-        exact (ra_to_fol_outFn_schema_mem_iff).mpr (right a)
+        exact (ra_to_fol_outFn_schema_mem_iff left).mpr (right a)
       · intro a
         obtain ⟨w, h⟩ := a
         split at h
@@ -117,5 +117,6 @@ noncomputable def ra_to_fol_def [FOL.folStruc] {dbs} (raQ : RA.Query) (h : raQ.i
         subst h_1
         use RelationSchema.fromIndex w
         apply And.intro (RelationSchema.fromIndex_mem w) rfl
+    case s a b pos sq ih => sorry
     all_goals sorry
 ⟩
---
+++ RelationalAlgebra/FOL/QueryAux.lean
@@ -0,0 +1,39 @@
+import RelationalAlgebra.FOL.Query
+
+open FOL FirstOrder Language Term
+
+namespace FOL
+
+/-- Maps bounded formulas along a map of terms and a map of relations. -/
+def BoundedQuery.mapTermRel {g : ℕ → ℕ} (ft : ∀ n, fol.Term (Variable ⊕ (Fin n)) → fol.Term (Variable ⊕ (Fin (g n))))
+    (h : ∀ n, BoundedQuery (g (n + 1)) → BoundedQuery (g n + 1)) :
+    ∀ {n}, BoundedQuery n → BoundedQuery (g n)
+  | _n, .R dbs rn vMap  => .R dbs rn (λ i => ft _ (vMap i))
+  | _n, .and q1 q2      => .and (q1.mapTermRel ft h) (q2.mapTermRel ft h)
+  | n,  .ex q           => (h n (q.mapTermRel ft h)).ex
+  -- | n,  .all q          => (h n (q.mapTermRel ft h)).all
+  -- | _n, .not q          => (q.mapTermRel ft h).not
+
+/-- Casts `L.BoundedFormula α m` as `L.BoundedFormula α n`, where `m ≤ n`. -/
+@[simp]
+def BoundedQuery.castLE : ∀ {m n : ℕ} (_h : m ≤ n), BoundedQuery m → BoundedQuery n
+  | _m, _n, h, .R dbs rn vMap => .R dbs rn (Term.relabel (Sum.map id (Fin.castLE h)) ∘ vMap)
+  | _m, _n, h, .and q₁ q₂ => (q₁.castLE h).and (q₂.castLE h)
+  | _m, _n, h, .ex q => (q.castLE (add_le_add_right h 1)).ex
+  -- | _m, _n, h, .all q => (q.castLE (add_le_add_right h 1)).all
+  -- | _m, _n, h, .not q => (q.castLE h).not
+
+/-- Raises all of the `Fin`-indexed variables of a formula greater than or equal to `m` by `n'`. -/
+def BoundedQuery.liftAt : ∀ {n : ℕ} (n' _m : ℕ), BoundedQuery n → BoundedQuery (n + n') :=
+  fun {_} n' m φ =>
+  φ.mapTermRel (fun _ t => t.liftAt n' m) fun _ =>
+    castLE (by rw [add_assoc, add_comm 1, add_assoc])
+
+/-- A function to help relabel the variables in bounded formulas. -/
+def BoundedQuery.relabelAux (g : Variable → Variable ⊕ (Fin n)) (k : ℕ) : Variable ⊕ (Fin k) → Variable ⊕ (Fin (n + k)) :=
+  Sum.map id finSumFinEquiv ∘ Equiv.sumAssoc _ _ _ ∘ Sum.map g id
+
+/-- Relabels a bounded formula's variables along a particular function. -/
+def BoundedQuery.relabel (g : Variable → Variable ⊕ (Fin n)) {k} (φ : BoundedQuery k) : BoundedQuery (n + k) :=
+  φ.mapTermRel (fun _ t => t.relabel (relabelAux g _)) fun _ =>
+    castLE (ge_of_eq (add_assoc _ _ _))
