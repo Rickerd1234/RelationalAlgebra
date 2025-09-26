bda16708b8b7e3475ec9cf1731f936cda9d4ca08
Rickerd1234 <rick.v.oosterhout@gmail.com>
2025-09-26 19:08:12 +0200

    Completed Projection proof :)

--- RelationalAlgebra/Equivalence/RAtoFOL/Projection.lean
+++ RelationalAlgebra/Equivalence/RAtoFOL/Projection.lean
@@ -2,97 +2,154 @@ import RelationalAlgebra.Equivalence.RAtoFOL.Conversion
 
 variable {dbi rs q} [FOL.folStruc dbi]
 
-theorem ra_to_fol_evalT.p_def.mp (h : RA.Query.isWellTyped dbi.schema (.p rs q))
+theorem ra_to_fol_evalT.p_def_eq (h : RA.Query.isWellTyped dbi.schema (.p rs q))
   (ih: (ra_to_fol_query q dbi.schema).evaluateT dbi = RA.Query.evaluateT dbi q) :
     (ra_to_fol_query (.p rs q) dbi.schema).evaluateT dbi = RA.Query.evaluateT dbi (.p rs q) := by
-      simp_all [FOL.Query.evaluateT, projectionT, ra_to_fol_query]
-      sorry
+      simp at h
+      obtain ⟨left, right⟩ := h
+      simp only [FOL.Query.evaluateT, ra_to_fol_query, FOL.Query.RealizeDom]
+      rw [← ra_to_fol_query_schema left] at right
+      rw [projectQuery.schema_def (ra_to_fol_query q dbi.schema) rs right]
+      simp [projectionT]
+      ext t
+      simp_all only [ra_to_fol_query.isWellTyped, Set.mem_setOf_eq]
 
-
---       intro t
---       simp only [RA.Query.isWellTyped.p_def, ra_to_fol_query, projectQuery.def, Nat.add_zero,
---         FOL.Query.RealizeDom.def, FOL.BoundedQuery.schema.exs_def, FOL.BoundedQuery.relabel_schema,
---         Finset.coe_pimage, RA.Query.evaluateT.p_def, projectionT, Set.mem_setOf_eq, and_imp] at ⊢ h
---       intro a_2 a_3
---       obtain ⟨left, right⟩ := h
---       have h_t_dom := by exact FOL.BoundedQuery.Realize.schema_sub_Dom a_2
---       simp [projectAttribute, *] at h_t_dom
---       have h_rs : ↑rs ⊆ t.Dom := by
---         rw [Set.subset_def]
---         intro x hx
---         simp_all only [FOL.Query.RealizeDom.def, ra_to_fol_query.isWellTyped,
---           ra_to_fol_query_schema, and_imp, forall_const, true_and, dite_not, Part.dom_iff_mem,
---           Finset.mem_coe, PFun.mem_dom]
---         exact h_t_dom x x (right hx) (by simp [hx])
---       use t.restrict h_rs
---       simp_all only [FOL.Query.RealizeDom.def, ra_to_fol_query.isWellTyped, ra_to_fol_query_schema,
---         and_imp, forall_const, projectAttribute, Finset.mem_sdiff, true_and, dite_not]
---       apply And.intro
---       · apply ih
---         . -- ???
---           have ⟨dropSet, h_dropSet⟩ : ∃s, (FOL.BoundedQuery.schema (ra_to_fol_query q dbi.schema) \ rs) = s := by simp
---           induction dropSet using Finset.induction_on with
---           | empty =>
---             rw [h_dropSet] at a_2
---             unfold projectAttribute at a_2
---             simp at a_2
---             have z : rs = q.schema dbi.schema := by simp_all; exact Finset.Subset.antisymm right h_dropSet
---             have h_rs' : ↑(FOL.BoundedQuery.schema (ra_to_fol_query q dbi.schema)) ⊆ PFun.Dom t := by simp_all
---             have z' := FOL.BoundedQuery.Realize.tuple_restrict2
---               (ra_to_fol_query.isWellTyped q dbi.schema left)
---               (h_rs')
---               a_2
---             have : (PFun.restrict t h_rs) = (PFun.restrict t h_rs') := by aesop
---             simp [this, z']
---           | insert h ih =>
---             rename_i a ds
---             sorry
---         . exact right
---       · intro a
---         apply And.intro
---         · intro a_1
---           ext v : 1
---           simp_all only [ra_to_fol_query.isWellTyped, PFun.mem_restrict, Finset.mem_coe, true_and]
---         · intro a_1
---           rw [Set.subset_def] at a_3
---           by_contra hc
---           simp [Part.ne_none_iff, Part.eq_some_iff, ← PFun.mem_dom] at hc
---           have h_image := a_3 a hc
---           simp [PFun.mem_image] at h_image
---           have ⟨a', ha', ha''⟩ := h_image
---           simp_all
---           by_cases hc' : a' ∈ rs
---           . simp_all
---           . simp_all
-
--- theorem ra_to_fol_evalT.p_def.mpr (h : RA.Query.isWellTyped dbi.schema (.p rs q))
---   (ih : RA.Query.isWellTyped dbi.schema q → ∀t ∈ RA.Query.evaluateT dbi q, (ra_to_fol_query q dbi.schema).RealizeDom dbi t) :
---     ∀t, t ∈ RA.Query.evaluateT dbi (.p rs q) → (ra_to_fol_query (.p rs q) dbi.schema).RealizeDom dbi t := by
---       intro t h_RA_eval
---       have t_Dom : t.Dom = rs := by
---         exact RA.Query.evaluate.validSchema (.p rs q) h t h_RA_eval
---       apply
---         FOL.Query.Realize.imp_RealizeDom_if_t_Dom_sub_schema
---           (ra_to_fol_query (.p rs q) dbi.schema)
---           (by simp_all)
-
---       simp only [ra_to_fol_query]
---       have : t = t.restrict (Set.Subset.antisymm_iff.mp t_Dom.symm).1 := by simp_all
---       rw [this]
---       apply projectQuery.Realize_restrict_def
---       . simp at h
---         obtain ⟨left, right⟩ := h
---         exact ra_to_fol_query.isWellTyped q dbi.schema left
---       . simp_all [projectionT]
---         obtain ⟨left, right⟩ := h
---         obtain ⟨w, h⟩ := h_RA_eval
---         obtain ⟨left_1, right_1⟩ := h
-
---         have : t = w := by
---           ext a v
---           by_cases a ∈ rs
---           . aesop
---           . sorry
---             -- ???
---         rw [this]
---         exact (ih w left_1).1
+      apply Iff.intro
+      · intro a
+        obtain ⟨left_1, right_1⟩ := a
+        obtain ⟨w, h⟩ := left_1
+        have ⟨dropSet, h_dropSet⟩ : ∃s, (FOL.BoundedQuery.schema (ra_to_fol_query q dbi.schema) \ rs) = s := by simp
+        induction dropSet using Finset.induction_on with
+        | empty =>
+          unfold projectAttribute at h
+          simp [h_dropSet] at h
+          rw [← ih]
+          simp
+          use t
+          simp_all only [Finset.sdiff_eq_empty_iff_subset, ra_to_fol_query.isWellTyped, implies_true, true_and]
+          apply And.intro
+          · apply And.intro
+            · apply (FOL.BoundedQuery.Realize.assignment_eq_ext ?_ ?_).mp h
+              . ext a v
+                exact Fin.elim0 a
+              . rfl
+            · exact fun ⦃a⦄ a_1 ↦ right (right_1 a_1)
+          · intro a a_1
+            apply Part.eq_none_iff'.mpr fun a ↦ a_1 (right_1 a)
+        | insert h' ih =>
+          rename_i a ds
+          simp_all only [ra_to_fol_query.isWellTyped, Finset.insert_eq_self, not_isEmpty_of_nonempty,
+            IsEmpty.forall_iff, and_true, IsEmpty.exists_iff, implies_true]
+          unfold projectAttribute at h
+          simp [h_dropSet] at h
+          rw [← ih]
+          simp
+          have : ∀a' ∈ insert a ds, a' ∈ FOL.BoundedQuery.schema (ra_to_fol_query q dbi.schema) \ rs := by
+            intro a_1 a_2
+            rw [h_dropSet]
+            exact a_2
+          use (Sum.elim t w ∘ fun a' ↦ if h : a' = a ∨ a' ∈ ds then Sum.inr (RM.RelationSchema.index (this a' (by simp_all))) else Sum.inl a')
+          simp_all only [ra_to_fol_query.isWellTyped, dite_eq_ite, Function.comp_apply]
+          apply And.intro
+          · apply And.intro
+            · apply (FOL.BoundedQuery.Realize.assignment_eq_ext ?_ ?_).mp h
+              . ext a v
+                exact Fin.elim0 a
+              . rfl
+            · rw [Set.subset_def]
+              intro x a_1
+              simp_all only [ra_to_fol_query.isWellTyped, PFun.mem_dom, Function.comp_apply, Finset.mem_coe]
+              obtain ⟨w_1, h_1⟩ := a_1
+              split at h_1
+              next h_2 =>
+                simp_all only [ra_to_fol_query.isWellTyped, Sum.elim_inr]
+                have := this x (by simp [h_2])
+                simp at this
+                exact this.1
+              next
+                h_2 =>
+                simp_all only [ra_to_fol_query.isWellTyped, Finset.mem_insert, or_false, not_or, Sum.elim_inl]
+                obtain ⟨left_1, right_2⟩ := h_2
+                simp_all only [ra_to_fol_query.isWellTyped, implies_true]
+                apply right
+                apply right_1
+                simp_all only [ra_to_fol_query.isWellTyped, PFun.mem_dom]
+                apply Exists.intro
+                · exact h_1
+          · intro a_1
+            split
+            next h_1 =>
+              simp_all only [ra_to_fol_query.isWellTyped, Sum.elim_inr]
+              cases h_1 with
+              | inl h_2 =>
+                subst h_2
+                apply And.intro
+                · intro a
+                  by_contra hc
+                  simp at this
+                  exact this.1.2 a
+                · intro a
+                  simp_all only [ra_to_fol_query.isWellTyped, Finset.mem_insert, or_false]
+                  exact Part.eq_none_iff'.mpr fun a_2 ↦ a (right_1 a_2)
+              | inr h_3 =>
+                apply And.intro
+                · intro a_2
+                  have : a_1 ∈ FOL.BoundedQuery.schema (ra_to_fol_query q dbi.schema) \ rs := by simp_all
+                  by_contra hc
+                  simp at this
+                  exact this.2 a_2
+                · intro a_2
+                  simp_all only [ra_to_fol_query.isWellTyped, Finset.mem_insert, or_false]
+                  apply Part.eq_none_iff'.mpr fun a ↦ a_2 (right_1 a)
+            next
+              h_1 =>
+              simp_all only [ra_to_fol_query.isWellTyped, Finset.mem_insert, or_false, not_or, Sum.elim_inl,
+                implies_true, true_and]
+              intro a_2
+              obtain ⟨left_1, right_2⟩ := h_1
+              apply Part.eq_none_iff'.mpr fun a ↦ a_2 (right_1 a)
+      · intro a
+        obtain ⟨w, h⟩ := a
+        obtain ⟨left_1, right_1⟩ := h
+        apply And.intro
+        · use λ i => w (RM.RelationSchema.fromIndex i)
+          rw [← ih] at left_1
+          simp at left_1
+          apply (FOL.BoundedQuery.Realize.assignment_eq_ext ?_ ?_).mp left_1.1
+          . ext a v
+            exact Fin.elim0 a
+          . obtain ⟨left_1, right_2⟩ := left_1
+            ext a v
+            simp_all only [ra_to_fol_query.isWellTyped, Function.comp_apply]
+            by_cases a ∈ rs
+            . simp_all only [ra_to_fol_query.isWellTyped, Finset.mem_sdiff, not_true_eq_false, and_false,
+                not_false_eq_true, projectAttribute_not_mem, Sum.elim_inl]
+            . unfold projectAttribute
+              simp_all
+              apply Iff.intro
+              · intro a_1
+                split
+                next h_1 =>
+                  simp_all only [ra_to_fol_query.isWellTyped, Finset.mem_sdiff, not_false_eq_true, and_self,
+                    Sum.elim_inr, RM.RelationSchema.fromIndex_index_eq]
+                next
+                  h_1 =>
+                  have : w.Dom ⊆ FOL.BoundedQuery.schema (ra_to_fol_query q dbi.schema) := by simp_all
+                  simp_all only [ra_to_fol_query.isWellTyped, Sum.elim_inl, not_false_eq_true, Part.not_mem_none]
+                  apply h_1
+                  apply this
+                  simp_all
+                  exact Exists.intro v a_1
+              · intro a_1
+                split at a_1
+                next h_1 =>
+                  simp_all only [ra_to_fol_query.isWellTyped, Finset.mem_sdiff, not_false_eq_true, and_self,
+                    Sum.elim_inr, RM.RelationSchema.fromIndex_index_eq]
+                next h_1 =>
+                  simp_all only [ra_to_fol_query.isWellTyped, Sum.elim_inl, not_false_eq_true, Part.not_mem_none]
+        · rw [Set.subset_def]
+          intro x a
+          simp_all only [PFun.mem_dom, Finset.mem_coe]
+          obtain ⟨w_1, h⟩ := a
+          by_contra hc
+          simp_all only [not_false_eq_true, Part.not_mem_none]
