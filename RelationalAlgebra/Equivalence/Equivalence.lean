8e38854d0ceca2223fe8b92b59db667a80aa6449
Rickerd1234 <rick.v.oosterhout@gmail.com>
2025-10-12 20:08:35 +0200

    Union and diff proofs

--- RelationalAlgebra/Equivalence/Equivalence.lean
+++ RelationalAlgebra/Equivalence/Equivalence.lean
@@ -14,7 +14,16 @@ theorem ra_to_fol_evalT {raQ dbi} [struc : FOL.folStruc dbi] (h : RA.Query.isWel
     | p rs sq ih => exact ra_to_fol_evalT.p_def_eq h (ih h.1)
     | j q₁ q₂ ih₁ ih₂ => exact ra_to_fol_evalT.j_def_eq h (ih₁ h.1) (ih₂ h.2)
     | r f q ih => exact ra_to_fol_evalT.r_def_eq h (ih h.1)
-    | _ => sorry
+    | u q₁ q₂ ih₁ ih₂ =>
+      simp_all [ra_to_fol_query, ra_to_fol_query_schema]
+      simp [FOL.Query.evaluateT, FOL.BoundedQuery.Realize, unionT, ← ih₁, ← ih₂]
+      simp_all [ra_to_fol_query_schema]
+      aesop
+    | d q nq ih nih =>
+      simp_all [ra_to_fol_query, ra_to_fol_query_schema]
+      simp [FOL.Query.evaluateT, FOL.BoundedQuery.Realize, diffT, ← ih, ← nih, Set.diff]
+      simp_all [ra_to_fol_query_schema]
+      aesop
 
 theorem ra_to_fol_eval {dbi} [struc : FOL.folStruc dbi] (raQ : RA.Query) (h_ra_wt : raQ.isWellTyped dbi.schema) :
   (ra_to_fol_query raQ dbi.schema).evaluate dbi = raQ.evaluate dbi h_ra_wt := by
