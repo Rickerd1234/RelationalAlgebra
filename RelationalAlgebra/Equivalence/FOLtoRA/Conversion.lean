c28cc690199ce7110db93fbb6f4fa856e7b245f0
Rickerd1234 <rick.v.oosterhout@gmail.com>
2025-10-20 01:03:19 +0200

    wip: Conversion toRA

--- RelationalAlgebra/Equivalence/FOLtoRA/Conversion.lean
+++ RelationalAlgebra/Equivalence/FOLtoRA/Conversion.lean
@@ -1,15 +1,22 @@
 import RelationalAlgebra.Equivalence.FOLtoRA.Adom
+import RelationalAlgebra.FOL.Query
+import Mathlib.ModelTheory.Complexity
 
-open FOL FirstOrder Language
+open RM FOL FirstOrder Language
 
 def toPrenex (q : FOL.BoundedQuery dbs n) : fol.BoundedFormula Attribute n :=
   q.toFormula.toPrenex
 
-def toRA : fol.BoundedFormula Attribute n → RA.Query
-  | .falsum => sorry
-  | .equal t₁ t₂ => .s sorry sorry (sorry)
+noncomputable def TermtoAtt (rs : RelationSchema) : fol.Term (Attribute ⊕ Fin (rs).card) → Attribute
+  | var (Sum.inl a) => a
+  | var (Sum.inr i) => RelationSchema.fromIndex i
+  | _ => Classical.arbitrary Attribute
+
+noncomputable def toRA (dbi : DatabaseInstance) [Fintype ↑(adomRs dbi.schema)] [Fintype ↑(adomAtts dbi.schema)] : fol.BoundedFormula Attribute n → RA.Query
+  | .falsum => .empty default
+  | .equal t₁ t₂ => .s (TermtoAtt (adomAtts dbi.schema).toFinset t₁) (TermtoAtt (adomAtts dbi.schema).toFinset t₂) (adom dbi)
   | .rel (.R dbs rn) ts => .R rn
-  | .imp f₁ ⊥ => .d sorry (toRA f₁)                 --  p → ⊥  = ¬p
-  | .imp (.not f₁) f₂ => .u (toRA f₁) (toRA f₂)     -- ¬p → q  =  p ∨ q
-  | .imp f₁ f₂ => .u (.d sorry (toRA f₁)) (toRA f₂) --  p → q  = ¬p ∨ q
-  | .all sf => .p (sf.freeVarFinset) (toRA sf)
+  | .imp f₁ ⊥ => .d (adom dbi) (toRA dbi f₁)                      --  p → ⊥  = ¬p
+  | .imp (.not f₁) f₂ => .u (toRA dbi f₁) (toRA dbi f₂)           -- ¬p → q  =  p ∨ q
+  | .imp f₁ f₂ => .u (.d (adom dbi) (toRA dbi f₁)) (toRA dbi f₂)  --  p → q  = ¬p ∨ q
+  | .all sf => .p (sf.freeVarFinset) (toRA dbi sf)
